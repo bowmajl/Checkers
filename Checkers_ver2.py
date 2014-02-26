6558776b6b17a16f269b7bd56fe39c3a1d396dc8
Munia337 <driessnp@mcmaster.ca>
2014-02-26 00:34:18 -0500

    New Code
    after first day, post in facebook if you edit so we dont overwrite

--- Checkers_ver2.py
+++ Checkers_ver2.py
@@ -1,6 +1,15 @@
 from graphics import *
 import sys
 
+#king pieces need to place kings not standard
+#only place 12 of each colour
+#add functionality to play button so program knows to exit custom board
+#when leaving custom setup erase white black and king buttons
+#maybe others, will get to it tomorrow most likely so post somewhere if you edit this
+
+
+
+
 class Board:
 
     
@@ -8,7 +17,10 @@ class Board:
         s.win = GraphWin('Checkers',600,600)   #draws screen
         s.win.setCoords(-1,-3,9,9)              #creates a coordinate system for the window
         s.ClearBoard()
-                
+        s.isCustom = False     
+        s.PlaceColour = 'White'
+        s.isKing = False
+
 
         letters = ['A','B','C','D','E','F','G','H',]
         for i in range(8):
@@ -41,29 +53,23 @@ class Board:
 ##        
         Standardtxt = Text(Point(0,-1.3),'Standard')  #These sets of 2 add the text to the buttons
         Standardtxt.draw(s.win)
-        BoardStxt = Text(Point(0,-1.7),'Board')
+        BoardStxt = Text(Point(0,-1.7),'Setup')
         BoardStxt.draw(s.win)
         Customtxt = Text(Point(0,-2.3),'Custom')  
         Customtxt.draw(s.win)
-        BoardCtxt = Text(Point(0,-2.7),'Board')
+        BoardCtxt = Text(Point(0,-2.7),'Setup')
         BoardCtxt.draw(s.win)        
         Cleartxt = Text(Point(2,-2.3),'Clear')
         Cleartxt.draw(s.win)
+        BoardCltxt = Text(Point(2,-2.7),'Board')
+        BoardCltxt.draw(s.win)
         Start_txt = Text(Point(2,-1.5),'Start!')
         Start_txt.draw(s.win)
-        BoardCltxt = Text(Point(2,-2.7),'Board')
-        BoardCltxt.draw(s.win)        
-
-
         
-##        WPtxt = Text(Point(0,-2.5),'White Piece')
-##        WPtxt.draw(s.win)
-##        BPtxt = Text(Point(2,-2.5),'Black Piece')
-##        BPtxt.draw(s.win)
-        
+ 
         s.SetPieces()
 
-    def SetPieces(s):
+    def SetPieces(s):   #so multiple clicks can happen without errors
         while True:
             s.Click()
 
@@ -75,13 +81,65 @@ class Board:
             s.StandardSetup()
         elif (1<=X<3 and Y == -3):  #if mouse click is in clear board button range
             s.ClearBoard()
-        elif (-1<=X<1 and Y == -3):
-        if (0<=X<8 and 0<=Y<8):  #if in board range
-            if (X%2 == 0 and Y%2 == 0) or (X%2 == 1 and Y%2 == 1): #if on red squares
-                s.P_array[X][Y] = Piece(s.win,X,Y,'White')
-                
-    def StandardSetup(s):
+        elif (-1<=X<1 and Y == -3):   #if click custom setup
+            s.CustomSetup()
+        elif (s.isCustom and s.PlaceColour == 'Black' and X==4 and Y==-2):   #if W is clicked
+            s.PlaceColour = 'White'
+        
+            ColourSquare(s.win,4,-2,'Green')
+            txt = Text(Point(4.5,-1.5),'W')
+            txt.draw(s.win)
+            
+            ColourSquare(s.win,5,-2,'Red')
+            txt = Text(Point(5.5,-1.5),'B')
+            txt.draw(s.win)
+        elif (s.isCustom and s.PlaceColour == 'White' and X==5 and Y==-2):  # if B is clicked
+            s.PlaceColour = 'Black'
+        
+            ColourSquare(s.win,4,-2,'Red')
+            txt = Text(Point(4.5,-1.5),'W')
+            txt.draw(s.win)
+            
+            ColourSquare(s.win,5,-2,'Green')
+            txt = Text(Point(5.5,-1.5),'B')
+            txt.draw(s.win)
+        elif (s.isCustom and s.setKing == False and X==6 and Y==-2): #if K is clicked and it is false already
+            s.setKing = True
+            ColourSquare(s.win,6,-2,'Green')
+            txt = Text(Point(6.5,-1.5),'K')
+            txt.draw(s.win)
+        elif (s.isCustom and s.setKing == True and X==6 and Y==-2): #if K is clicked and it is true already
+            s.setKing = False
+            ColourSquare(s.win,6,-2,'Red')
+            txt = Text(Point(6.5,-1.5),'K')
+            txt.draw(s.win)
+        elif (0<=X<8 and 0<=Y<8 and s.isCustom):                    #within range of board
+            if (X%2 == 0 and Y%2 == 0) or (X%2 == 1 and Y%2 == 1):  #only on red squares
+                s.P_array[X][Y] = Piece(s.win,X,Y,s.PlaceColour)    #updates that square in array
+
+    def CustomSetup(s):         #gives functionality to the custom button
+        s.isCustom = True       #selects custom mode
+        s.PlaceColour = 'White' #defaults the next colour to white
+        s.setKing = False       #defaults to not a king
+        
+        ColourSquare(s.win,4,-2,'Green')   #makes white button green, and others red below
+        txt = Text(Point(4.5,-1.5),'W')
+        txt.draw(s.win)
+        
+        ColourSquare(s.win,5,-2,'Red')
+        txt = Text(Point(5.5,-1.5),'B')
+        txt.draw(s.win)
+        
+        ColourSquare(s.win,6,-2,'Red')
+        txt = Text(Point(6.5,-1.5),'K')
+        txt.draw(s.win)
+
+        while True
+            s.Click()                   #this needs to be changed for when to end
+                        
+    def StandardSetup(s):       #defines the standard button
         s.ClearBoard()
+        s.isCustom = False       #not in custom mode
         for j in range(8):
             for i in range(8):
                 if ((i%2 == 0 and j%2 == 0) or (i%2 == 1 and j%2 == 1)) and (j < 3):
@@ -120,12 +178,12 @@ class Board:
 
         #play()
             
-def ColourRect(win,Xmin,Ymin,Xmax,Ymax,colour):
+def ColourRect(win,Xmin,Ymin,Xmax,Ymax,colour):        #function to create a rectangle with a given colour and size
     rect = Rectangle(Point(Xmin,Ymin),Point(Xmax,Ymax))
     rect.setFill(colour)
     rect.draw(win)                
 def ColourSquare(win,X,Y,colour):
-    sqr = Rectangle(Point(X,Y),Point(X+1,Y+1))
+    sqr = Rectangle(Point(X,Y),Point(X+1,Y+1))      #makes a 1x1 square with a given colour
     sqr.setFill(colour)
     sqr.draw(win)
 
@@ -133,16 +191,9 @@ def ExitGame(win):
     win.close()
     sys.exit()
 
-##def initialize_twodlist(foo):
-##    twod_list = []
-##    new = []
-##    for i in range (8):
-##        for j in range (8):
-##            new.append(foo)
-##        twod_list.append(new)
-##        new = []
 
-class Piece:
+
+class Piece:                                #creates a piece
     def __init__(s,win,X,Y,colour):
         s.x = X
         s.y = Y
@@ -153,27 +204,8 @@ class Piece:
         s.circ.setFill(colour)
 
 
+
     
-##def Click(s):
-##    click = s.win.getMouse()
-##    X, Y = s.ClickedSquare(click)
-##    if X == s.x-1 and (Y == -2 or Y == -1):
-##        s.disarmClick = not s.disarmClick
-##        s.UpdateDisarmSpace()
-##    elif s.disarmClick:
-##        s.disarmClick = False
-##        s.UpdateDisarmSpace()
-##        s.DisarmClicked(X,Y)
-##    elif s.squares[X][Y] != 10 and X >= 0 and Y >= 0:
-##        s.TriggerSquare(X,Y)
-
-
-
-
-
-
-
-        
         
 board = Board()
 
